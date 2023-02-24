981eb75fcea636c1d6f465122608814bdc9e7f16
zzeong <zzeong315@gamil.com>
2023-02-24 17:24:50 +0900

    fix: Overview 수정

--- src/router/Price.tsx
+++ src/router/Price.tsx
@@ -1,12 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
-import { useRecoilValue } from 'recoil';
-import styled from 'styled-components';
-import { fetchCoinTickers } from '../api';
-import { coinIdAtom } from '../atom';
-import { PriceData } from '../types';
-import { Overview } from './Coin';
+import { useQuery } from "@tanstack/react-query";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { fetchCoinTickers } from "../api";
+import { coinIdAtom } from "../atom";
+import { PriceData } from "../types";
+import { Loader } from "./Coins";
 
-const PriceContainer = styled(Overview)`
+const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
   height: 350px;
 `;
 const LeftPrice = styled.div`
@@ -36,7 +41,7 @@ const PercentBox = styled.div`
 `;
 const Time = styled.span`
   &:hover {
-    color: ${props => props.theme.accentColor};
+    color: ${(props) => props.theme.accentColor};
   }
 `;
 const Percent = styled(Time)``;
@@ -47,58 +52,69 @@ const TimeUnit = styled.span`
 const PercentUnit = styled(TimeUnit)``;
 const Price = () => {
   const coinId = useRecoilValue(coinIdAtom);
-  const {isLoading, data} = useQuery<PriceData>(["tickers",coinId], () => fetchCoinTickers(coinId!));
+  const { isLoading, data } = useQuery<PriceData>(["tickers", coinId], () =>
+    fetchCoinTickers(coinId!)
+  );
   return (
-    <PriceContainer>
-      <LeftPrice>
-        <Unit>USD </Unit>
-        <PriceText>$ {data?.quotes.USD.price.toFixed(3)}</PriceText>
-      </LeftPrice>
-      <RightPrice>
-        <RightTitle>Percent Change<br/> Over Time</RightTitle>
-        <PercentBox>
-          <TimeUnit>Time</TimeUnit>
-          <PercentUnit>%</PercentUnit>
-        </PercentBox>
-        <PercentBox>
-          <Time>15M</Time>
-          <Percent>{data?.quotes.USD.percent_change_15m}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>30M</Time>
-          <Percent>{data?.quotes.USD.percent_change_30m}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>1H</Time>
-          <Percent>{data?.quotes.USD.percent_change_1h}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>6H</Time>
-          <Percent>{data?.quotes.USD.percent_change_6h}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>12H</Time>
-          <Percent>{data?.quotes.USD.percent_change_12h}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>24H</Time>
-          <Percent>{data?.quotes.USD.percent_change_24h}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>7D</Time>
-          <Percent>{data?.quotes.USD.percent_change_7d}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>30D</Time>
-          <Percent>{data?.quotes.USD.percent_change_30d}</Percent>
-        </PercentBox>
-        <PercentBox>
-          <Time>1Y</Time>
-          <Percent>{data?.quotes.USD.percent_change_1y}</Percent>
-        </PercentBox>
-      </RightPrice>
-    </PriceContainer>
-  )
-}
+    <>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <PriceContainer>
+          <LeftPrice>
+            <Unit>USD </Unit>
+            <PriceText>$ {data?.quotes.USD.price.toFixed(3)}</PriceText>
+          </LeftPrice>
+          <RightPrice>
+            <RightTitle>
+              Percent Change
+              <br /> Over Time
+            </RightTitle>
+            <PercentBox>
+              <TimeUnit>Time</TimeUnit>
+              <PercentUnit>%</PercentUnit>
+            </PercentBox>
+            <PercentBox>
+              <Time>15M</Time>
+              <Percent>{data?.quotes.USD.percent_change_15m}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>30M</Time>
+              <Percent>{data?.quotes.USD.percent_change_30m}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>1H</Time>
+              <Percent>{data?.quotes.USD.percent_change_1h}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>6H</Time>
+              <Percent>{data?.quotes.USD.percent_change_6h}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>12H</Time>
+              <Percent>{data?.quotes.USD.percent_change_12h}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>24H</Time>
+              <Percent>{data?.quotes.USD.percent_change_24h}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>7D</Time>
+              <Percent>{data?.quotes.USD.percent_change_7d}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>30D</Time>
+              <Percent>{data?.quotes.USD.percent_change_30d}</Percent>
+            </PercentBox>
+            <PercentBox>
+              <Time>1Y</Time>
+              <Percent>{data?.quotes.USD.percent_change_1y}</Percent>
+            </PercentBox>
+          </RightPrice>
+        </PriceContainer>
+      )}
+    </>
+  );
+};
 
 export default Price;
