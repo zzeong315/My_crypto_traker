6bd4d232c5de9541a0291ee5b52f2e2ab4cb6eff
zzeong <zzeong315@gamil.com>
2023-02-24 17:02:23 +0900

    fix: route 수정 다시

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,10 +5,10 @@ import Coins from './router/Coins';
 const Router = () => {
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Coins />}/>
-        <Route path="/:coinId/*" element={<Coin />}/>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Coins />}/>
+        <Route path={`${process.env.PUBLIC_URL}/:coinId/*`} element={<Coin />}/>
       </Routes>
     </BrowserRouter>
   )
